fb55182a8399b1f80bc5374cb8b66a575dcf58d1
sanikadongre <sanika.dongre@colorado.edu>
2019-03-27 17:02:11 -0600

    led

---
+++ main/bbgled.c
@@ -0,0 +1,71 @@
+/*********************
+*LED task
+*Author: Sanika Dongre
+*Date created: 03/25/19
+***********************/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LED0_LOC "/sys/class/leds/beaglebone:green:usr0/brightness"
+#define LED0_INIT_LOC "/sys/class/leds/beaglebone:green:usr0/trigger"
+
+void init_led(void)
+{
+	FILE* fdptr = fopen(LED0_INIT_LOC, "w+");
+	if(fdptr==NULL)
+	{
+		perror("error in file open\n");		
+		exit(-1);
+	}
+	else
+	{
+		fprintf(fdptr,"default");
+		fclose(fdptr);
+		
+	}
+}
+
+void on_led(void)
+{
+	FILE *fdptr = fopen(LED0_LOC, "w+");
+	if(fdptr==NULL)
+	{
+		perror("error in file open\n");		
+		exit(-1);
+	}
+	else
+	{
+		fputc('1',fdptr);
+		fclose(fdptr);
+	}
+	
+}
+
+void off_led(void)
+{
+	FILE* fdptr = fopen(LED0_LOC, "w+");
+	if(fdptr==NULL)
+	{
+		perror("error in file open\n");
+		exit(-1);
+	}
+	else
+	{
+		fputc('0', fdptr);
+		fclose(fdptr);
+		
+	}
+	
+}
+
+void main()
+{
+	init_led();
+	on_led();
+	usleep(500);
+	off_led();
+	usleep(500);
+	on_led();
+}
