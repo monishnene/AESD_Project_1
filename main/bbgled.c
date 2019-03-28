86cdff488fa69883cc5b859e0883d96c1b251ab2
monishnene <monish.nene@colorado.edu>
2019-03-28 13:19:51 -0600

    led fixed

--- main/bbgled.c
+++ main/bbgled.c
@@ -11,54 +11,72 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
-#define  STR_SIZE 30
+#define STR_SIZE 30
+#define	LED_COUNT 4
+
+uint8_t *path[]={"/sys/class/gpio/gpio53/value","/sys/class/gpio/gpio54/value","/sys/class/gpio/gpio55/value","/sys/class/gpio/gpio56/value"};
 
 void led_on(uint8_t led)
 {
-	uint8_t* str=malloc(STR_SIZE);
-	sprintf(str,"/sys/class/gpio/gpio%d/value",led);
-	FILE* fptr=fopen(str,"w");
-	uint8_t data='1',error=0;
-	error=fwrite(&data,1,1,fptr);
-	free(str);
-	fclose(fptr);
+	if(led<LED_COUNT)
+	{
+		FILE* fptr=fopen(path[led],"w");
+		uint8_t data='1',error=0;
+		error=fwrite(&data,1,1,fptr);
+		fclose(fptr);
+	}
+	else
+	{
+		printf("LED index error\n");
+	}
+	return;
 }
 
 void led_off(uint8_t led)
 {
-	uint8_t* str=malloc(STR_SIZE);
-	sprintf(str,"/sys/class/gpio/gpio%d/value",led);
-	FILE* fptr=fopen(str,"w");
-	uint8_t data='0',error=0;
-	error=fwrite(&data,1,1,fptr);
-	free(str);
-	fclose(fptr);
+	if(led<LED_COUNT)
+	{
+		FILE* fptr=fopen(path[led],"w");
+		uint8_t data='0',error=0;
+		error=fwrite(&data,1,1,fptr);
+		fclose(fptr);
+	}
+	else
+	{
+		printf("LED index error\n");
+	}
+	return;
 }
 
 void led_toggle(uint8_t led)
 {
-	uint8_t* str=malloc(STR_SIZE);
-	sprintf(str,"/sys/class/gpio/gpio%d/value",led);
-	FILE* fptr=fopen(str,"w+");
-	uint8_t data='1',error=0,prev=0;
-	error=fread(&prev,1,1,fptr);
-	data=(prev=='0')?'1':'0';
-	error=fwrite(&data,1,1,fptr);
-	free(str);
-	fclose(fptr);
+	if(led<LED_COUNT)
+	{
+		FILE* fptr=fopen(path[led],"w+");
+		uint8_t data='1',error=0,prev=0;
+		error=fread(&prev,1,1,fptr);
+		data=(prev=='0')?'1':'0';
+		error=fwrite(&data,1,1,fptr);
+		fclose(fptr);
+	}
+	else
+	{
+		printf("LED index error\n");
+	}
+	return;
 }
 
 void main()
 {
-	uint8_t led=53,i=0;
-	led_off(53);	
-	led_off(54);		
-	led_off(55);		
-	led_off(56);
+	uint8_t led=0,i=0;
+	led_off(0);	
+	led_off(1);		
+	led_off(2);		
+	led_off(3);
 	for(i=0;i<255;i++)
 	{
 		usleep(1e5);
 		led_toggle(led);		
-		led=(led==56)?53:(led+1);
+		led=(led==LED_COUNT-1)?0:(led+1);
 	}
 }
