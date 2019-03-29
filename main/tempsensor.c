0ae5853656bf304ba2bfab11c558a4471031e09b
sanikadongre <sanika.dongre@colorado.edu>
2019-03-29 10:01:09 -0600

    temp

--- main/tempsensor.c
+++ main/tempsensor.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <linux/i2c-dev.h>
 #include <stdint.h>
 #include <time.h>
