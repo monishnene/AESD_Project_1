6ffcda3f59bdc0290fc17fb2a99256166ebc4580
sanikadongre <sanika.dongre@colorado.edu>
2019-03-30 10:10:16 -0600

    final light working

--- main/lightsensor.c
+++ main/lightsensor.c
@@ -25,9 +25,10 @@ void i2c_file(int32_t fd)
 	fd=open("/dev/i2c-2", O_RDWR);
 	ioctl(fd, I2C_SLAVE, LUX_SLAVE_ADDR);
 }
+
 void i2c_write(int32_t fd,uint8_t regval)
 {
-	if(write(fd, &regval, sizeof(regval))<0)
+	if(write(fd, &regval, sizeof(regval))!=sizeof(regval))
 	{
 		perror("write function has been failed");
 	}
@@ -38,191 +39,65 @@ int32_t i2c_read(int32_t fd,uint8_t* buffer,uint32_t size)
 	return read(fd, buffer, size);
 }
 
-uint8_t cmdreg_write(int32_t fd)
-{
-	if(write(fd, &START_COMMAND, 1) < 0)
-	{
-		perror("error in write function");
-		exit(-1);
-	}
-}
-void control_reg_write(int32_t fd)
-{
-	i2c_write(fd,POWER_ADDR);
-	i2c_write(fd,POWER_ON_CMD);
-}
-
-uint8_t control_reg_read(int32_t fd)
-{
-	int32_t error=0;
-	uint8_t powerval=0;
-	error=i2c_read(fd,&powerval,1);
-	if(powerval==POWER_ON_CMD)
-	{
-		return powerval;
-		
-	}
-	else return 0;
-}
-
-void id_reg_write(int32_t fd)
-{
-	i2c_write(fd,ID_REGISTER);
-
-}
-uint8_t id_reg_read(int32_t fd)
-{
-	uint8_t sensor_id=0;
-	int32_t error=0;
-	error=i2c_read(fd,&sensor_id,1);
-	if(sensor_id==ID_VALUE)
-	{
-		return sensor_id;
-	}
-	else return 0;
-}
-
-void timing_reg_write(int32_t fd)
-{
-	i2c_write(fd,TIMING_REG);
-	i2c_write(fd,TIMING_VAL);
-}
-
-uint8_t timing_reg_read(int32_t fd)
-{
-	int32_t error=0;
-	uint8_t timer=0;
-	error=i2c_read(fd,&timer,1);
-	if(timer==TIMING_VAL)
-	{
-		return timer;
-	}
-	else return 0;
-}
-
-void int_control_reg_write(int32_t fd)
-{
-	i2c_write(fd,INTERRUPT_REG);
-	i2c_write(fd,INTERRUPT_VALUE);
-}
-
-uint8_t int_control_reg_read(int32_t fd)
-{
-	int32_t error=0;
-	uint8_t interr=0;
-	error=i2c_read(fd,&interr,1);
-	if(interr==INTERRUPT_VALUE)
-	{
-		return interr;
-	}
-	else return 0;
-}
-
-void int_threshold_write(int32_t fd, int *data_buff)
-{
-	uint8_t value_ob;
-	value_ob = TLL;
-	write(fd, &value_ob,1) ;
-	value_ob = data_buff[0];
-	write(fd, &value_ob, 1);
-	value_ob = TLH;
-  	write(fd, &value_ob, 1);
-	value_ob = data_buff[1];
-	write(fd, &value_ob, 1);
-	value_ob = THL;
-	write(fd, &value_ob, 1);
-	value_ob = data_buff[2];
- 	write(fd, &value_ob, 1);
-	value_ob = THH;
-	write(fd, &value_ob, 1);
-	value_ob = data_buff[3];
-	write(fd, &value_ob, 1);
-}
-
-int* int_threshold_read(int fd, int *data_buff)
-{
-	uint8_t value_ob;
-	value_ob = TLL;
-	write(fd,&value_ob,1);
-	read(fd, &value_ob,1) ;
-	data_buff[0] = value_ob;
-	value_ob = TLH;
-	write(fd, &value_ob, 1);
-	read(fd,&value_ob,1);
-	data_buff[1]= value_ob;
-	value_ob = THL;
-	write(fd,&value_ob,1);
-  	read(fd, &value_ob, 1);
-	data_buff[2]= value_ob;
-	value_ob = THH;
-	write(fd,&value_ob,1);
-	read(fd,&value_ob,1);
-	data_buff[3] = value_ob;
-	return data_buff;
-}
-
-uint16_t channel0_read(int fd)
-{
-	uint8_t lsbval;	
-	uint16_t msbval, outval;
-	int value_ob;
-	value_ob =  CH0_L;
-	write(fd, &value_ob, 1);
-	read(fd, &lsbval, 1);
-	value_ob =  CH0_H;
-  	write(fd, &value_ob, 1);
-	read(fd, &msbval, 1);
-	outval = msbval<<8 | lsbval;
-	return outval;
-}
-
-uint16_t channel1_read(int fd)
-{
-	int value_ob;
-	uint8_t lsbval;
-	uint16_t msbval, outval;
-	value_ob =  CH1_L;
-	write(fd, &value_ob, 1);
-  	read(fd, &lsbval, 1);
-	value_ob =  CH1_H;
-	write(fd, &value_ob, 1);
-	read(fd, &msbval, 1);
-	outval = msbval<<8 | lsbval;
-	return outval;
-}
-
-void poweroff_func(int32_t fd)
-{
-	uint8_t sensor_id;	
-	sensor_id=0;
-	i2c_write(fd,POWER_ADDR);
-	i2c_write(fd,POWER_OFF_CMD);
-	i2c_write(fd,ID_REGISTER);
-	error=i2c_read(fd,&sensor_id,1);
-	if(sensor_id!=ID_VALUE)
-	{
-		printf("LUX power off\n");
-	}	
-}
-
-static int16_t get_luminosity()
+float get_luminosity()
 {
 	uint8_t sensor_id=0, powerval=0, timer=0;
 	int32_t error=0,fd=0;
-	uint8_t ch0_l=0,ch1_l=0,ch0_h=0,ch1_h=0;
+	uint8_t databuff=1, dataop;
+	uint8_t regval;
+	uint16_t ch0_l=0,ch1_l=0,ch0_h=0,ch1_h=0;
 	uint16_t ch0=0,ch1=0;
 	float adcval=0.0;
 	int16_t lux_output=0;
+	//i2c init
+	fd=open("/dev/i2c-2", O_RDWR);
+	ioctl(fd, I2C_SLAVE, LUX_SLAVE_ADDR);	
+	//power on	
+	i2c_write(fd,POWER_ADDR);
+	i2c_write(fd,POWER_ON_CMD);
+	error=i2c_read(fd,&powerval,1);
+	if(powerval==POWER_ON_CMD)
+	{
+		printf("the value of power is %x\n", powerval);
+	}
 	//read channels
-	i2c_write(fd,CH0_L);
-	i2c_read(fd,&ch0_l,1);
-	i2c_write(fd,CH0_H);
-	i2c_read(fd,&ch0_h,1);
-	i2c_write(fd,CH1_L);
-	i2c_read(fd,&ch1_l,1);
-	i2c_write(fd,CH1_H);
-	i2c_read(fd,&ch1_h,1);	
-	printf("ch0h=%d,ch0l=%d,ch1h=%d,ch1l=%d\n",ch0_h,ch0_l,ch1_h,ch1_l);
+	uint8_t addr = 0x8C;
+	if(write(fd,&addr,1)!=1)
+	{
+		perror("errror in write ch0l\n");
+	}
+	if(read(fd,&ch0_l,1)!=1)
+	{
+		perror("error in read ch0l\n");
+	}
+	addr=0x8D;
+	if(write(fd,&addr,1)!=1)
+	{
+		perror("error in write ch0h\n");
+	}
+	if(read(fd,&ch0_h,1)!=1)
+	{
+		perror("error in read choh\n");
+	}
+	addr=0x8E;
+	if(write(fd,&addr,1)!=1)
+	{
+		perror("error in write ch1l\n");
+	}
+	if(read(fd,&ch1_l,1)!=1)
+	{
+		perror("error in read ch1li\n");
+	}
+	addr=0x8F;
+	if(write(fd,&addr,1)!=1)
+	{
+		perror("error in write ch1h\n");
+	}
+	if(read(fd,&ch1_h,1)!=1)
+	{
+		perror("error in read ch1h\n");
+	}
+	printf("ch0l=%d,ch1l=%d, ch0h=%d, ch1h=%d\n",ch0_l,ch1_l, ch0_l,ch0_h);
 	ch1=(ch1_h<<8)|ch1_l;
 	ch0=(ch0_h<<8)|ch0_l;
 	adcval = (float)ch1/(float)ch0;	
@@ -246,44 +121,14 @@ static int16_t get_luminosity()
     	else
 	{
 		lux_output=0;
-	}
-	//power off
-	poweroff_func(fd);
-	//file open	
-	return (int16_t)lux_output;
+	}	
+	return lux_output;
 }
 
 int main()
 {
-	int16_t lux_output;	
+	float lux_output;
 	int32_t fd;
-	int s_output[4] = {10,1,2,3};
-	int* readop;
-	uint8_t powerval=0,sensor_id=0, timer=0, interr=0;
-	//i2c init
-	i2c_file(fd);	
-	//power on	
-	control_reg_write(fd);
-	powerval = control_reg_read(fd);
-	printf("the value of power is %x\n", powerval);
-	//identification register
-	sensor_id= id_reg_read(fd);
-	printf("LUX power on sensor_id=%x\n",sensor_id);
-	//timing reg
-	timing_reg_write(fd);
-	timer= timing_reg_read(fd);
-	printf("the timer value is %x\n",timer);
-	//interrupt control reg
-	int_control_reg_write(fd);
-	interr=int_control_reg_read(fd);
-	printf(" The interrupt control register value is %x\n", interr);
-	//interrupt threshold registers
-	int_threshold_write(fd, s_output);
-	readop = int_threshold_read(fd, s_output);
-	for(int i=0; i<4; i++)
-	{
-		printf("The output threshold buffer is %d\n", *(readop+i));
-	}
 	lux_output = get_luminosity(fd);
 	printf("The obtained lux value is %f\n", lux_output);
 }
